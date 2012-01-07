edfa4e53e8eb42320d9a0fd6785b2cc668db8221
Russ Cox <rsc@swtch.com>
2012-01-07 13:29:22 -0500

    keychain: better error from UserPasswd

--- keychain/doc.go
+++ keychain/doc.go
@@ -8,10 +8,22 @@ package keychain
 
 // BUG(rsc): Package keychain is only implemented on OS X.
 
+import (
+	"fmt"
+)
+
 // UserPasswd returns the user name and password for authenticating
 // to the named server.  If the user argument is non-empty, UserPasswd
 // restricts its search to passwords for the named user.
 func UserPasswd(server, preferredUser string) (user, passwd string, err error) {
-	return userPasswd(server, preferredUser)
+	user, passwd, err = userPasswd(server, preferredUser)
+	if err != nil {
+		if preferredUser != "" {
+			err = fmt.Errorf("loading password for %s@%s: %v", preferredUser, server, err)
+		} else {
+			err = fmt.Errorf("loading password for %s: %v", server, err)
+		}
+	}
+	return
 }
 
