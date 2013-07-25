0245e2cc54ba54e2feb91edc1203fb5c8785627a
Russ Cox <rsc@swtch.com>
2013-07-24 23:26:12 -0400

    issue: new acme mode -a

--- cmd/issue/issue.go
+++ cmd/issue/issue.go
@@ -1,3 +1,7 @@
+// Copyright 2013 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
@@ -7,7 +11,7 @@ import (
 	"flag"
 	"fmt"
 	"html"
-	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -17,14 +21,36 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"code.google.com/p/rsc/oauthprompt"
 )
 
+var auth struct {
+	APIClientID     string
+	APIClientSecret string
+}
+
+const Version = "Go1.2"
+
+var aflag = flag.Bool("a", false, "run in acme mode")
+var project = flag.String("p", "go", "code.google.com project identifier")
+var v = flag.Bool("v", false, "verbose")
+var version = flag.String("dash", "", "Label for dashboard, like \"Go1.2\"")
+var quick = flag.Bool("quick", false, "use cached xml from disk")
+var xmlflag = flag.Bool("xml", false, "dump xml")
+
 func usage() {
-	fmt.Fprintf(os.Stderr, `usage: issue [-p project] query
+	fmt.Fprintf(os.Stderr, `usage: issue [-a] [-dash Go1.2] [-p project] [query]
 
 If query is a single number, prints the full history for the issue.
 Otherwise, prints a table of matching results.
 The special query 'go1' is shorthand for 'Priority-Go1'.
+
+The -a flag runs as an Acme window, making the query optional.
+
+The -dash mode generates an issue dashboard for a Go release.
+It maintains a collection of output files named for the release:
+go12.html, go12.graph, and so on.
 `)
 	os.Exit(2)
 }
@@ -42,12 +68,15 @@ type Entry struct {
 	Author    struct {
 		Name string `xml:"name"`
 	} `xml:"author"`
-	Owner  string   `xml:"owner>username"`
-	Status string   `xml:"status"`
-	Label  []string `xml:"label"`
+	Owner      string   `xml:"owner>username"`
+	Status     string   `xml:"status"`
+	Label      []string `xml:"label"`
+	MergedInto string   `xml:"mergedInto"`
+	CC         []string `xml:"cc>username"`
 
-	Dir    string
-	Number int
+	Dir      string
+	Number   int
+	Comments []Entry
 }
 
 func (e Entry) IsStatus(s string) bool { return e.Status == s }
@@ -77,17 +106,21 @@ func (e Entries) Len() int           { return len(e) }
 func (e Entries) Swap(i, j int)      { e[i], e[j] = e[j], e[i] }
 func (e Entries) Less(i, j int) bool { return e[i].Title < e[j].Title }
 
-var project = flag.String("p", "go", "code.google.com project identifier")
-var v = flag.Bool("v", false, "verbose")
-var go11flag = flag.Bool("go11", false, "go11 web page update")
-var quick = flag.Bool("quick", false, "use cached xml from disk")
-
 func main() {
 	flag.Usage = usage
 	flag.Parse()
+	log.SetFlags(0)
 
-	if *go11flag {
-		go11()
+	if *version != "" {
+		dash(*version)
+		return
+	}
+
+	if *aflag {
+		if err := login(); err != nil {
+			log.Fatal(err)
+		}
+		acmeMode()
 		return
 	}
 
@@ -106,11 +139,99 @@ func main() {
 		q = "label:Priority-Go1"
 	}
 
-	log.SetFlags(0)
+	data, err := fetch(q, full)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	if full {
+		printFull(os.Stdout, data)
+	} else {
+		printList(os.Stdout, data)
+	}
+}
+
+type Change struct {
+	Summary string
+	Status  string
+	Owner   string
+	Label   []string
+	CC      []string
+	Comment string
+}
+
+func write(id int, ch *Change) error {
+	var buf bytes.Buffer
+	buf.WriteString(`<?xml version='1.0' encoding='UTF-8'?>
+<entry xmlns='http://www.w3.org/2005/Atom' xmlns:issues='http://schemas.google.com/projecthosting/issues/2009'>
+  <content type='html'>`)
+	xml.Escape(&buf, []byte(ch.Comment))
+	buf.WriteString(`</content>
+  <author>
+    <name>ignored</name>
+  </author>
+  <issues:updates>
+`)
+	tag := func(t, data string) {
+		buf.WriteString(`    ` + t)
+		xml.Escape(&buf, []byte(data))
+		buf.WriteString(`</` + t[1:])
+	}
+
+	if ch.Summary != "" {
+		tag("<issues:summary>", ch.Summary)
+	}
+	if ch.Status != "" {
+		status := ch.Status
+		merge := ""
+		if strings.HasPrefix(status, "Duplicate ") {
+			merge = strings.TrimPrefix(status, "Duplicate ")
+			status = "Duplicate"
+		}
+		tag("<issues:status>", status)
+		if merge != "" {
+			tag("<issues:mergedInto>", merge)
+		}
+	}
+	if ch.Owner != "" {
+		tag("<issues:ownerUpdate>", ch.Owner)
+	}
+	for _, l := range ch.Label {
+		tag("<issues:label>", l)
+	}
+	for _, cc := range ch.CC {
+		tag("<issues:ccUpdate>", cc)
+	}
+	buf.WriteString(`
+  </issues:updates>
+</entry>
+`)
+
+	// Done with XML!
+
+	u := "https://code.google.com/feeds/issues/p/" + *project + "/issues/" + fmt.Sprint(id) + "/comments/full"
+	req, err := http.NewRequest("POST", u, &buf)
+	if err != nil {
+		return fmt.Errorf("write: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/atom+xml")
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("write: %v", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 201 {
+		buf.Reset()
+		io.Copy(&buf, resp.Body)
+		return fmt.Errorf("write: %v\n%s", resp.Status, buf.String())
+	}
+	return nil
+}
+
+func fetch(q string, full bool) ([]Entry, error) {
 	query := url.Values{
 		"q":           {q},
-		"max-results": {"600"},
+		"max-results": {"1000"},
 	}
 	if !full {
 		query["can"] = []string{"open"}
@@ -119,37 +240,29 @@ func main() {
 	if *v {
 		log.Print(u)
 	}
-	r, err := http.Get(u)
+	r, err := client.Get(u)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	if *xmlflag {
+		io.Copy(os.Stdout, r.Body)
+		os.Exit(0)
 	}
 
 	var feed Feed
 	if err := xml.NewDecoder(r.Body).Decode(&feed); err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	r.Body.Close()
 
 	sort.Sort(feed.Entry)
-	for _, e := range feed.Entry {
-		id := e.ID
-		if i := strings.Index(id, "id="); i >= 0 {
-			id = id[:i+len("id=")]
-		}
-		fmt.Printf("%s\t%s\n", id, e.Title)
-		if full {
-			fmt.Printf("Reported by %s (%s)\n", e.Author.Name, e.Published.Format("2006-01-02 15:04:05"))
-			if e.Owner != "" {
-				fmt.Printf("\tOwner: %s\n", e.Owner)
-			}
-			if e.Status != "" {
-				fmt.Printf("\tStatus: %s\n", e.Status)
-			}
-			for _, l := range e.Label {
-				fmt.Printf("\tLabel: %s\n", l)
-			}
-			if e.Content != "" {
-				fmt.Printf("\n\t%s\n", wrap(html.UnescapeString(e.Content), "\t"))
+	if full {
+		for i := range feed.Entry {
+			e := &feed.Entry[i]
+			id := e.ID
+			if i := strings.Index(id, "id="); i >= 0 {
+				id = id[:i+len("id=")]
 			}
 			u := "https://code.google.com/feeds/issues/p/" + *project + "/issues/" + id + "/comments/full"
 			if *v {
@@ -157,36 +270,81 @@ func main() {
 			}
 			r, err := http.Get(u)
 			if err != nil {
-				log.Fatal(err)
+				return nil, err
 			}
 
 			var feed Feed
 			if err := xml.NewDecoder(r.Body).Decode(&feed); err != nil {
-				log.Fatal(err)
+				return nil, err
 			}
 			r.Body.Close()
+			e.Comments = feed.Entry
+		}
+	}
 
-			for _, e := range feed.Entry {
-				fmt.Printf("\n%s (%s)\n", e.Title, e.Published.Format("2006-01-02 15:04:05"))
-				for _, up := range e.Updates {
-					if up.Summary != "" {
-						fmt.Printf("\tSummary: %s\n", up.Summary)
-					}
-					if up.Owner != "" {
-						fmt.Printf("\tOwner: %s\n", up.Owner)
-					}
-					if up.Status != "" {
-						fmt.Printf("\tStatus: %s\n", up.Status)
-					}
-					if up.Label != "" {
-						fmt.Printf("\tLabel: %s\n", up.Label)
-					}
+	return feed.Entry, nil
+}
+
+func printFull(w io.Writer, entries []Entry) {
+	for _, e := range entries {
+		id := e.ID
+		if i := strings.Index(id, "id="); i >= 0 {
+			id = id[:i+len("id=")]
+		}
+		fmt.Fprintf(w, "Summary: %s\n", e.Title)
+		fmt.Fprintf(w, "Status: %s", e.Status)
+		if e.Status == "Duplicate" {
+			fmt.Fprintf(w, " %s", e.MergedInto)
+		}
+		fmt.Fprintf(w, "\n")
+		fmt.Fprintf(w, "Owner: %s\n", e.Owner)
+		fmt.Fprintf(w, "CC:")
+		for _, cc := range e.CC {
+			fmt.Fprintf(w, " %s", cc)
+		}
+		fmt.Fprintf(w, "\n")
+		fmt.Fprintf(w, "Labels:")
+		for _, l := range e.Label {
+			fmt.Fprintf(w, " %s", l)
+		}
+		fmt.Fprintf(w, "\n")
+		fmt.Fprintf(w, "\n")
+
+		fmt.Fprintf(w, "Reported by %s (%s)\n", e.Author.Name, e.Published.Format("2006-01-02 15:04:05"))
+		if e.Content != "" {
+			fmt.Fprintf(w, "\n\t%s\n", wrap(html.UnescapeString(e.Content), "\t"))
+		}
+
+		for _, e := range e.Comments {
+			fmt.Fprintf(w, "\n%s (%s)\n", e.Title, e.Published.Format("2006-01-02 15:04:05"))
+			for _, up := range e.Updates {
+				if up.Summary != "" {
+					fmt.Fprintf(w, "\tSummary: %s\n", up.Summary)
 				}
-				if e.Content != "" {
-					fmt.Printf("\n\t%s\n", wrap(html.UnescapeString(e.Content), "\t"))
+				if up.Owner != "" {
+					fmt.Fprintf(w, "\tOwner: %s\n", up.Owner)
+				}
+				if up.Status != "" {
+					fmt.Fprintf(w, "\tStatus: %s\n", up.Status)
+				}
+				if up.Label != "" {
+					fmt.Fprintf(w, "\tLabel: %s\n", up.Label)
 				}
 			}
+			if e.Content != "" {
+				fmt.Fprintf(w, "\n\t%s\n", wrap(html.UnescapeString(e.Content), "\t"))
+			}
 		}
+	}
+}
+
+func printList(w io.Writer, entries []Entry) {
+	for _, e := range entries {
+		id := e.ID
+		if i := strings.Index(id, "id="); i >= 0 {
+			id = id[:i+len("id=")]
+		}
+		fmt.Fprintf(w, "%s\t%s\n", id, e.Title)
 	}
 }
 
@@ -213,308 +371,26 @@ func wrap(t string, prefix string) string {
 	return out
 }
 
-// go11 web page update
+var client = http.DefaultClient
 
-type ByDir []Entry
-
-func (x ByDir) Len() int      { return len(x) }
-func (x ByDir) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
-
-func (x ByDir) Less(i, j int) bool {
-	if x[i].Dir != x[j].Dir {
-		return x[i].Dir < x[j].Dir
+func login() error {
+	if false {
+		data, err := ioutil.ReadFile("../../authblob")
+		if err != nil {
+			return err
+		}
+		if err := json.Unmarshal(data, &auth); err != nil {
+			return err
+		}
 	}
-	return x[i].Number < x[j].Number
-}
 
-type Point struct {
-	Time      time.Time
-	Go11      int
-	Go11Maybe int
-}
+	auth.APIClientID = "993255737644.apps.googleusercontent.com"
+	auth.APIClientSecret = "kjB02zudLVECBmJdKVMaZluI"
 
-func (p Point) JDate() template.JS {
-	yy, mm, dd := p.Time.Date()
-	h, m, s := p.Time.Clock()
-	return template.JS(fmt.Sprintf("new Date(%d, %d, %d, %d, %d, %d)", yy, mm-1, dd, h, m, s))
-}
-
-func go11() {
-	data, err := ioutil.ReadFile("go11.graph")
+	tr, err := oauthprompt.GoogleToken(".token-code.google.com", auth.APIClientID, auth.APIClientSecret, "https://code.google.com/feeds/issues")
 	if err != nil {
-		log.Fatal(err) // in wrong directory
+		return err
 	}
-	var graph []Point
-	if err := json.Unmarshal(data, &graph); err != nil {
-		log.Fatal(err)
-	}
-
-	var data1, data2 []byte
-	if *quick {
-		data1, err = ioutil.ReadFile("go11.xml")
-		if err != nil {
-			log.Fatal(err)
-		}
-		data2, err = ioutil.ReadFile("go11maybe.xml")
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		r, err := http.Get("https://code.google.com/feeds/issues/p/go/issues/full?can=open&q=label:Go1.1&max-results=600")
-		if err != nil {
-			log.Fatal(err)
-		}
-		data1, err = ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		r.Body.Close()
-		r, err = http.Get("https://code.google.com/feeds/issues/p/go/issues/full?can=open&q=label:Go1.1Maybe&max-results=600")
-		if err != nil {
-			log.Fatal(err)
-		}
-		data2, err = ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		r.Body.Close()
-		ioutil.WriteFile("go11.xml", data1, 0666)
-		ioutil.WriteFile("go11maybe.xml", data2, 0666)
-	}
-
-	var feed Feed
-	if err := xml.Unmarshal(data1, &feed); err != nil {
-		log.Fatal(err)
-	}
-	n1 := len(feed.Entry)
-	if err := xml.Unmarshal(data2, &feed); err != nil {
-		log.Fatal(err)
-	}
-	n2 := len(feed.Entry) - n1
-
-	if !*quick {
-		graph = append(graph, Point{time.Now(), n1, n2})
-		buf, err := json.Marshal(graph)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err := ioutil.WriteFile("go11.graph", buf, 0666); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	for i := range feed.Entry {
-		e := &feed.Entry[i]
-		e.Number, _ = strconv.Atoi(e.ID)
-		dir := e.Title
-		if i := strings.Index(dir, ":"); i >= 0 {
-			dir = dir[:i]
-		}
-		if i := strings.Index(dir, ","); i >= 0 {
-			dir = dir[:i]
-		}
-		e.Dir = dir
-	}
-
-	sort.Sort(ByDir(feed.Entry))
-
-	var groups [][]Entry
-	dir := ""
-	for _, e := range feed.Entry {
-		if e.Dir != dir {
-			dir = e.Dir
-			groups = append(groups, nil)
-		}
-		n := len(groups) - 1
-		groups[n] = append(groups[n], e)
-	}
-
-	var small []Point
-	now := time.Now()
-	day := -1
-	for _, p := range graph {
-		if p.Go11Maybe == 0 {
-			continue
-		}
-		d := p.Time.Day()
-		if d != day || now.Sub(p.Time) < 3*24*time.Hour {
-			day = d
-			small = append(small, p)
-		}
-	}
-
-	tmpl := template.Must(
-		template.New("main").
-			Funcs(template.FuncMap{"labeledGroup": labeledGroup}).
-			Parse(go11template),
-	)
-	var all = struct {
-		Graph []Point
-		Issue [][]Entry
-	}{
-		Graph: small,
-		Issue: groups,
-	}
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, &all); err != nil {
-		log.Fatal(err)
-	}
-	if err := ioutil.WriteFile("go11.html", buf.Bytes(), 0666); err != nil {
-		log.Fatal(err)
-	}
+	client = tr.Client()
+	return nil
 }
-
-func labeledGroup(g []Entry, s string) string {
-	if strings.HasPrefix(s, "-") {
-		for _, e := range g {
-			if t := e.Labeled(s[1:]); t != "" {
-				return ""
-			}
-		}
-		return "ok"
-	}
-		
-	for _, e := range g {
-		if t := e.Labeled(s); t != "" {
-			return t
-		}
-	}
-	return ""
-}
-
-var go11template = `<html>
-  <head>
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawCharts);
-      function drawCharts() {
-        var data = new google.visualization.DataTable();
-        data.addColumn('datetime', 'Date');
-        data.addColumn('number', 'Go 1.1');
-        data.addColumn('number', 'Go 1.1 + Maybe');
-        var one = 1;
-        data.addRows([
-{{range .Graph}}          [{{.JDate}}, {{.Go11}}, {{.Go11}}+{{.Go11Maybe}}],
-{{end}}
-        ])
-        var options = {
-          width: 800, height: 400,
-          title: 'Go 1.1 Issues',
-          strictFirstColumnType: true,
-          vAxis: {minValue: 0, maxValue: 299},
-          vAxes: {0: {title: 'Open Issues'}}
-        };
-        var chart = new google.visualization.AreaChart(document.getElementById('open_div'));
-        chart.draw(data, options);
-      }
-    </script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script>
-      var onlySuggest = false;
-      var hideMaybe = true;
-      function rehide() {
-        $("tr").show();
-        if(onlySuggest) {
-          $("tr.nosuggest").hide();
-        }
-        if(hideMaybe) {
-          $("tr.maybe").hide();
-          $("tr.maybex").hide();
-        }
-        
-        if(onlySuggest && !hideMaybe) {
-          $("#suggest").html("suggested issues only");
-        } else {
-          $("#suggest").html("<a href='javascript:dosuggest()'>show suggested issues only</a>");
-        }
-        if(!onlySuggest && hideMaybe) {
-          $("#go11").html("Go1.1 issues only");
-        } else {
-          $("#go11").html("<a href='javascript:dogo11()'>show Go 1.1 issues only</a>");
-        }
-        if(!onlySuggest && !hideMaybe) {
-          $("#go11maybe").html("all issues");
-        } else {
-          $("#go11maybe").html("<a href='javascript:dogo11maybe()'>show all issues</a>");
-        }
-      }
-      function dosuggest() {
-        onlySuggest = true;
-        hideMaybe = false;
-        window.location.hash = "s";
-        rehide();
-      }
-      function dogo11() {
-        onlySuggest = false;
-        hideMaybe = true;
-        window.location.hash = "";
-        rehide();
-      }
-      function dogo11maybe() {
-        onlySuggest = false;
-        hideMaybe = false;
-        window.location.hash = "m";
-        rehide();
-      }
-      function start() {
-        if (window.location.hash == "s" || window.location.hash == "#s") {
-          dosuggest();
-        } else if (window.location.hash == "m" || window.location.hash == "#m") {
-          dogo11maybe();
-        } else {
-          dogo11();
-        }
-      }
-    </script>
-    
-    <style>
-      td.dir {font-weight: bold;}
-      td.suggest {padding-left: 1em;}
-      .size {font-family: sans-serif; font-size: 70%; text-align: center;}
-      tr.maybe {color: #aaa;}
-      tr.suggest {}
-      h1 {font-size: 120%;}
-      a {color: #000;}
-      tr.maybe a {color: #aaa;}
-      .key, .key td {font-family: sans-serif; font-size: 90%;}
-    </style>
-  </head>
-
-  <body onload="start()">
-    <h1>Go 1.1: Open Issues</h1>
-
-    <div id="open_div"></div>
-    
-    <div class="key">
-    Key:
-    <table>
-    <tr><td class="suggest"><td class="size">S</td><td>small change: less than 30 minutes (e.g. doc fix)
-    <tr><td class="suggest"><td class="size">M</td><td>medium change: less than 2 hours (e.g. small feature/fix + tests)
-    <tr><td class="suggest"><td class="size">L</td><td>large change: less than 8 hours
-    <tr><td class="suggest"><td class="size">XL</td><td>extra large change: more than one day
-    <tr><td class="suggest"><td>&#x261e;</td><td>suggested for people looking for work
-    </table>
-    </div>
-    <br><br>
-    
-    <span id="suggest"></span> | <span id="go11"></span> | <span id="go11maybe"></span>
-
-    <br><br>
-    <table>
-    {{range .Issue}}
-      <tr class="{{if labeledGroup . "-Go1.1Maybe"}}maybex{{else}}nomaybe{{end}} {{if labeledGroup . "Suggested"}}suggest{{else}}nosuggest{{end}}"><td class="dir" colspan="4">{{(index . 0).Dir}}
-      {{range .}}
-        <tr class="{{if .Labeled "Go1.1Maybe"}}maybe{{else}}nomaybe{{end}} {{if .Labeled "Suggested"}}suggest{{else}}nosuggest{{end}}">
-          <td class="suggest">{{if .Labeled "Suggested"}}&#x261e;{{end}}
-          <td class="size">{{.Labeled "Size-"}}
-          <td class="num">{{.Number}}
-          <td class="title"><a href="http://golang.org/issue/{{.Number}}">{{.Title}}</a>
-            {{if .Labeled "Go1.1Maybe"}}[maybe]{{end}}
-            {{if .IsStatus "Started"}}[<i>started by {{.Owner}}</i>]{{end}}
-      {{end}}
-    {{end}}
-    </table>
-  </body>
-</html>
-`
