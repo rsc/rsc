4f49ff8bfd8463e8e7b9e74d850e4e067ff8cecc
Russ Cox <rsc@golang.org>
2014-07-18 00:59:56 -0400

    c2go: printf up next

--- c2go/liblink/data.go
+++ c2go/liblink/data.go
@@ -1,116 +1,6 @@
 package main
 
-import (
-	"encoding/binary"
-	"math"
-)
-
-func addaddr(ctxt *Link, s *LSym, t *LSym) int64 {
-	return addaddrplus(ctxt, s, t, 0)
-}
-
-func addaddrplus(ctxt *Link, s *LSym, t *LSym, add int64) int64 {
-	var i int
-	var r *Reloc
-	if s.typ == 0 {
-		s.typ = int(SDATA)
-	}
-	s.reachable = 1
-	i = s.size
-	s.size += ctxt.arch.ptrsize
-	symgrow(ctxt, s, int64(s.size))
-	r = addrel(s)
-	r.sym = t
-	r.off = i
-	r.siz = uint8(ctxt.arch.ptrsize)
-	r.typ = int(R_ADDR)
-	r.add = add
-	return int64(i) + int64(r.siz)
-}
-
-func addaddrplus4(ctxt *Link, s *LSym, t *LSym, add int64) int64 {
-	var i int
-	var r *Reloc
-	if s.typ == 0 {
-		s.typ = int(SDATA)
-	}
-	s.reachable = 1
-	i = s.size
-	s.size += 4
-	symgrow(ctxt, s, int64(s.size))
-	r = addrel(s)
-	r.sym = t
-	r.off = i
-	r.siz = 4
-	r.typ = int(R_ADDR)
-	r.add = add
-	return int64(i) + int64(r.siz)
-}
-
-func addpcrelplus(ctxt *Link, s *LSym, t *LSym, add int64) int64 {
-	var i int
-	var r *Reloc
-	if s.typ == 0 {
-		s.typ = int(SDATA)
-	}
-	s.reachable = 1
-	i = s.size
-	s.size += 4
-	symgrow(ctxt, s, int64(s.size))
-	r = addrel(s)
-	r.sym = t
-	r.off = i
-	r.add = add
-	r.typ = int(R_PCREL)
-	r.siz = 4
-	return int64(i) + int64(r.siz)
-}
-
-func addrel(s *LSym) *Reloc {
-	s.r = append(s.r, Reloc{})
-	return &s.r[len(s.r)-1]
-}
-
-func addsize(ctxt *Link, s *LSym, t *LSym) int64 {
-	var i int
-	var r *Reloc
-	if s.typ == 0 {
-		s.typ = int(SDATA)
-	}
-	s.reachable = 1
-	i = s.size
-	s.size += ctxt.arch.ptrsize
-	symgrow(ctxt, s, int64(s.size))
-	r = addrel(s)
-	r.sym = t
-	r.off = i
-	r.siz = uint8(ctxt.arch.ptrsize)
-	r.typ = int(R_SIZE)
-	return int64(i) + int64(r.siz)
-}
-
-func adduint16(ctxt *Link, s *LSym, v uint16) int64 {
-	return int64(adduintxx(ctxt, s, uint64(v), 2))
-}
-
-func adduint32(ctxt *Link, s *LSym, v uint32) int64 {
-	return int64(adduintxx(ctxt, s, uint64(v), 4))
-}
-
-func adduint64(ctxt *Link, s *LSym, v uint64) int64 {
-	return int64(adduintxx(ctxt, s, v, 8))
-}
-
-func adduint8(ctxt *Link, s *LSym, v uint8) int64 {
-	return int64(adduintxx(ctxt, s, uint64(v), 1))
-}
-
-func adduintxx(ctxt *Link, s *LSym, v uint64, wid int) int {
-	var off int
-	off = s.size
-	setuintxx(ctxt, s, int64(off), v, int64(wid))
-	return off
-}
+import "math"
 
 // Derived from Inferno utils/6l/obj.c and utils/6l/span.c
 // http://code.google.com/p/inferno-os/source/browse/utils/6l/obj.c
@@ -142,11 +32,25 @@ func adduintxx(ctxt *Link, s *LSym, v uint64, wid int) int {
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
+
 func mangle(file string) {
 	sysfatal("%s: mangled input file", file)
 }
 
-var endian binary.ByteOrder
+func symgrow(ctxt *Link, s *LSym, lsiz int64) {
+	var siz int
+	siz = int(lsiz)
+	if int64(siz) != lsiz {
+		sysfatal("symgrow size %d too long", lsiz)
+	}
+	if len(s.p) >= siz {
+		return
+	}
+	for cap(s.p) < siz {
+		s.p = append(s.p[:cap(s.p)], 0)
+	}
+	s.p = s.p[:siz]
+}
 
 func savedata(ctxt *Link, s *LSym, p *Prog, pn string) {
 	var off int
@@ -164,9 +68,9 @@ func savedata(ctxt *Link, s *LSym, p *Prog, pn string) {
 		switch siz {
 		default:
 		case 4:
-			endian.PutUint32(s.p[off:], math.Float32bits(float32(p.to.u.dval)))
+			ctxt.arch.byteOrder.PutUint32(s.p[off:], math.Float32bits(float32(p.to.u.dval)))
 		case 8:
-			endian.PutUint64(s.p[off:], math.Float64bits(p.to.u.dval))
+			ctxt.arch.byteOrder.PutUint64(s.p[off:], math.Float64bits(p.to.u.dval))
 		}
 	} else {
 		if p.to.typ == ctxt.arch.D_SCONST {
@@ -192,11 +96,11 @@ func savedata(ctxt *Link, s *LSym, p *Prog, pn string) {
 				case 1:
 					s.p[off] = byte(o)
 				case 2:
-					endian.PutUint16(s.p[off:], uint16(o))
+					ctxt.arch.byteOrder.PutUint16(s.p[off:], uint16(o))
 				case 4:
-					endian.PutUint32(s.p[off:], uint32(o))
+					ctxt.arch.byteOrder.PutUint32(s.p[off:], uint32(o))
 				case 8:
-					endian.PutUint64(s.p[off:], uint64(o))
+					ctxt.arch.byteOrder.PutUint64(s.p[off:], uint64(o))
 				}
 			} else {
 				if p.to.typ == ctxt.arch.D_ADDR {
@@ -215,8 +119,113 @@ func savedata(ctxt *Link, s *LSym, p *Prog, pn string) {
 	}
 }
 
-func setaddr(ctxt *Link, s *LSym, off int64, t *LSym) int64 {
-	return setaddrplus(ctxt, s, int(off), t, 0)
+func addrel(s *LSym) *Reloc {
+	s.r = append(s.r, Reloc{})
+	return &s.r[len(s.r)-1]
+}
+
+func setuintxx(ctxt *Link, s *LSym, off int64, v uint64, wid int64) int64 {
+	if s.typ == 0 {
+		s.typ = int(SDATA)
+	}
+	s.reachable = 1
+	if int64(s.size) < off+wid {
+		s.size = int(off + wid)
+		symgrow(ctxt, s, int64(s.size))
+	}
+	switch wid {
+	case 1:
+		s.p[off] = uint8(v)
+		break
+	case 2:
+		ctxt.arch.byteOrder.PutUint16(s.p[off:], uint16(v))
+	case 4:
+		ctxt.arch.byteOrder.PutUint32(s.p[off:], uint32(v))
+	case 8:
+		ctxt.arch.byteOrder.PutUint64(s.p[off:], uint64(v))
+	}
+	return off + wid
+}
+
+func adduintxx(ctxt *Link, s *LSym, v uint64, wid int) int {
+	var off int
+	off = s.size
+	setuintxx(ctxt, s, int64(off), v, int64(wid))
+	return off
+}
+
+func adduint8(ctxt *Link, s *LSym, v uint8) int64 {
+	return int64(adduintxx(ctxt, s, uint64(v), 1))
+}
+
+func adduint16(ctxt *Link, s *LSym, v uint16) int64 {
+	return int64(adduintxx(ctxt, s, uint64(v), 2))
+}
+
+func adduint32(ctxt *Link, s *LSym, v uint32) int64 {
+	return int64(adduintxx(ctxt, s, uint64(v), 4))
+}
+
+func adduint64(ctxt *Link, s *LSym, v uint64) int64 {
+	return int64(adduintxx(ctxt, s, v, 8))
+}
+
+func setuint8(ctxt *Link, s *LSym, r int, v uint8) int64 {
+	return setuintxx(ctxt, s, int64(r), uint64(v), 1)
+}
+
+func setuint16(ctxt *Link, s *LSym, r int, v uint16) int64 {
+	return setuintxx(ctxt, s, int64(r), uint64(v), 2)
+}
+
+func setuint32(ctxt *Link, s *LSym, r int, v uint32) int64 {
+	return setuintxx(ctxt, s, int64(r), uint64(v), 4)
+}
+
+func setuint64(ctxt *Link, s *LSym, r int, v uint64) int64 {
+	return setuintxx(ctxt, s, int64(r), v, 8)
+}
+
+func addaddrplus(ctxt *Link, s *LSym, t *LSym, add int64) int64 {
+	var i int
+	var r *Reloc
+	if s.typ == 0 {
+		s.typ = int(SDATA)
+	}
+	s.reachable = 1
+	i = s.size
+	s.size += ctxt.arch.ptrsize
+	symgrow(ctxt, s, int64(s.size))
+	r = addrel(s)
+	r.sym = t
+	r.off = i
+	r.siz = uint8(ctxt.arch.ptrsize)
+	r.typ = int(R_ADDR)
+	r.add = add
+	return int64(i) + int64(r.siz)
+}
+
+func addpcrelplus(ctxt *Link, s *LSym, t *LSym, add int64) int64 {
+	var i int
+	var r *Reloc
+	if s.typ == 0 {
+		s.typ = int(SDATA)
+	}
+	s.reachable = 1
+	i = s.size
+	s.size += 4
+	symgrow(ctxt, s, int64(s.size))
+	r = addrel(s)
+	r.sym = t
+	r.off = i
+	r.add = add
+	r.typ = int(R_PCREL)
+	r.siz = 4
+	return int64(i) + int64(r.siz)
+}
+
+func addaddr(ctxt *Link, s *LSym, t *LSym) int64 {
+	return addaddrplus(ctxt, s, t, 0)
 }
 
 func setaddrplus(ctxt *Link, s *LSym, off int, t *LSym, add int64) int64 {
@@ -238,56 +247,42 @@ func setaddrplus(ctxt *Link, s *LSym, off int, t *LSym, add int64) int64 {
 	return int64(off) + int64(r.siz)
 }
 
-func setuint16(ctxt *Link, s *LSym, r int, v uint16) int64 {
-	return setuintxx(ctxt, s, int64(r), uint64(v), 2)
+func setaddr(ctxt *Link, s *LSym, off int64, t *LSym) int64 {
+	return setaddrplus(ctxt, s, int(off), t, 0)
 }
-
-func setuint32(ctxt *Link, s *LSym, r int, v uint32) int64 {
-	return setuintxx(ctxt, s, int64(r), uint64(v), 4)
-}
-
-func setuint64(ctxt *Link, s *LSym, r int, v uint64) int64 {
-	return setuintxx(ctxt, s, int64(r), v, 8)
-}
-
-func setuint8(ctxt *Link, s *LSym, r int, v uint8) int64 {
-	return setuintxx(ctxt, s, int64(r), uint64(v), 1)
-}
-
-func setuintxx(ctxt *Link, s *LSym, off int64, v uint64, wid int64) int64 {
+func addsize(ctxt *Link, s *LSym, t *LSym) int64 {
+	var i int
+	var r *Reloc
 	if s.typ == 0 {
 		s.typ = int(SDATA)
 	}
 	s.reachable = 1
-	if int64(s.size) < off+wid {
-		s.size = int(off + wid)
-		symgrow(ctxt, s, int64(s.size))
-	}
-	switch wid {
-	case 1:
-		s.p[off] = uint8(v)
-		break
-	case 2:
-		endian.PutUint16(s.p[off:], uint16(v))
-	case 4:
-		endian.PutUint32(s.p[off:], uint32(v))
-	case 8:
-		endian.PutUint64(s.p[off:], uint64(v))
-	}
-	return off + wid
+	i = s.size
+	s.size += ctxt.arch.ptrsize
+	symgrow(ctxt, s, int64(s.size))
+	r = addrel(s)
+	r.sym = t
+	r.off = i
+	r.siz = uint8(ctxt.arch.ptrsize)
+	r.typ = int(R_SIZE)
+	return int64(i) + int64(r.siz)
 }
 
-func symgrow(ctxt *Link, s *LSym, lsiz int64) {
-	var siz int
-	siz = int(lsiz)
-	if int64(siz) != lsiz {
-		sysfatal("symgrow size %d too long", lsiz)
+func addaddrplus4(ctxt *Link, s *LSym, t *LSym, add int64) int64 {
+	var i int
+	var r *Reloc
+	if s.typ == 0 {
+		s.typ = int(SDATA)
 	}
-	if len(s.p) >= siz {
-		return
-	}
-	for cap(s.p) < siz {
-		s.p = append(s.p[:cap(s.p)], 0)
-	}
-	s.p = s.p[:siz]
+	s.reachable = 1
+	i = s.size
+	s.size += 4
+	symgrow(ctxt, s, int64(s.size))
+	r = addrel(s)
+	r.sym = t
+	r.off = i
+	r.siz = 4
+	r.typ = int(R_ADDR)
+	r.add = add
+	return int64(i) + int64(r.siz)
 }
